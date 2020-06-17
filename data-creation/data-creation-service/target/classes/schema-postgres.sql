a6e34badf7ed75b0e97b81ecf7bc2dd752c89605
prakhar_dwivedi <dwivedi.prakhar82@gmail.com>
2020-06-17 12:00:03 +0530

    removed unwanted folders

--- data-creation/data-creation-service/target/classes/schema-postgres.sql
+++
@@ -1,7 +0,0 @@
-/*DROP TABLE product_details;*/
-create table product_details (store_id varchar(50) NOT NULL, store_country varchar(100), store_location VARCHAR(50),
-    product_category varchar(50),product_id varchar(50),discount varchar(50) ,mrp varchar(50) ,cp varchar(50), sp varchar(50),
-    transaction_date varchar(50)
-);
-
-
--- exchange-rate/exchange-rate-service/target/generated-sources/restclient/com/poc/restclient/auth/OAuthFlow.java
+++
@@ -1,5 +0,0 @@
-package com.poc.restclient.auth;
-
-public enum OAuthFlow {
-    accessCode, implicit, password, application
-}
